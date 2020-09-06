121d718584252d94d76fa74cf512459fe8df04fc
yerinshin <user-email>
2020-09-06 15:07:38 +0900

    update blogproject

--- blogproject2/urls.py
+++ blogproject2/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+import blog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', blog.views.home, name = "home"),
+
     path('blog/', include('blog.urls')),
     path('portfolio/', include('portfolio.urls')),
 
